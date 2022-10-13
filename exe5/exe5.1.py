3aaa1dd68ebf6c3f65c0c89a6209ea40d2f50140
Oleg <you@example.com>
2022-10-13 14:09:00 +0700

    exe5 check

--- exe5/exe5.1.py
+++ exe5/exe5.1.py
@@ -1,3 +1,9 @@
 #Задание 5.1
 str = input("Введите текст:").replace("абв","")
 print("Текст без 'абв':",str)
+
+#правильное решение
+PATTERN = 'абв'
+my_text = 'арбуз абвгд был абвгд красным'
+
+print(' '.join([word for word in my_text.split() if not PATTERN in word]))
