22cae4459f5f564e12ea24b7da630333ea5146e8
Oleg <you@example.com>
2022-11-10 07:27:47 +0700

    exe9.2 ready

--- exe9/telegram/bot_commands.py
+++ exe9/telegram/bot_commands.py
@@ -1,5 +1,27 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Привет {update.effective_user.first_name}')
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Команды:\n/hello\n/sum\n/mult\n/power\n/start')
+
+async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text
+    items = msg.split()
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} + {y} = {x+y}')
+
+async def mult_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    items = update.message.text.split()
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} * {y} = {x*y}')
+
+async def power_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    items = update.message.text.split()
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} ^ {y} = {x**y}')
--- exe9/telegram/telbot.py
+++ exe9/telegram/telbot.py
@@ -1,7 +1,14 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
+from bot_commands import *
 
 app = ApplicationBuilder().token("1903908474:AAGU-PLODiwJ4fJGNE73SRDH7BvN51n6lds").build()
 
-app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("hello", hello_command))
+app.add_handler(CommandHandler("start", start_command))
+app.add_handler(CommandHandler("sum", sum_command))
+app.add_handler(CommandHandler("mult", mult_command))
+app.add_handler(CommandHandler("power", power_command))
+
+
 
 app.run_polling()
